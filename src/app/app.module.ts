75279c72829a1b092aeb51f6ee211ca921370484
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-13 11:26:36 -0500

    feat: Inputs, inputs en login, imagen renderizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// - Angular Material
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+// - Componentes
 import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
